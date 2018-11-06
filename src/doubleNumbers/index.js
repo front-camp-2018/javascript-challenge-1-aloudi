6d0671cc6e23c8289df905988440fcfe1faefa8d
unknown <aloudi86@gmail.com>
2018-11-06 14:02:01 +0200

    challenges done

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,26 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
+  const isDoubleNum = num => {
+    const numToString = String(num);
+    const numberOfDigits = numToString.length;
+    const evenDigitsNumber = (numberOfDigits % 2) ? false : true;
+    if (evenDigitsNumber) {
+      const substringA = numToString.substr(0, numberOfDigits / 2);
+      const substringB = numToString.substr(numberOfDigits / 2);
+      if (substringA === substringB) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false
+    }
+  }
+
+  if (isDoubleNum(num)) {
+    return num;
+  } else {
+    return num * 2;
+  }
   // your logic here...
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,43 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
+
+  const findWeight = (entry) => {
+    const numArray = entry.split('');
+    const weight = numArray.reduce((accumulator, currentValue) => {
+      return Number(accumulator) + Number(currentValue);
+    }, 0)
+    return weight;
+  }
+
+  const mergeSort = (arr) => {
+    if (arr.length === 1) {
+      return arr
+    }
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+    return merge(
+      mergeSort(left),
+      mergeSort(right)
+    )
+  }
+
+  const merge = (left, right) => {
+    let result = [];
+    let indexLeft = 0;
+    let indexRight = 0;
+    while (indexLeft < left.length && indexRight < right.length) {
+      if (findWeight(left[indexLeft]) < findWeight(right[indexRight])) {
+        result.push(left[indexLeft]);
+        indexLeft++;
+      } else {
+        result.push(right[indexRight]);
+        indexRight++;
+      }
+    }
+    return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+  }
+
+  return mergeSort(arr);
   // your logic here...
 };
